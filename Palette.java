6388d82bc83a7e81d523c81990f778b13af6481c
Jeroen Vranken <jeroen.vranken@student.uva.nl>
2015-03-06 12:42:36 +0100

    more colors no exclamation

--- Palette.java
+++ Palette.java
@@ -7,15 +7,17 @@ public class Palette {
 	Color lightOrange;
 	Color white;
 	Color lightGreen;
+	Color darkGreen;
 	Color red;
 	
 	Palette(){
-		green = new Color(0x19A347);
+		green = new Color(0x2FBF22);
 		orange = new Color(0xFABB32);
 		lightOrange = new Color(0xFFCB59);
 		white = Color.WHITE;
 		lightGreen = new Color(0x1FED64);
 		red = Color.red;
+		darkGreen = new Color(0x24911A);
 	}
 
 }
