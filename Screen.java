fbf10d6b43b2483a11eb415259eb78f08bdc69a4
gvanhorn <gijsvanhorn@gmail.com>
2015-03-11 11:05:44 +0100

    errors removed

--- Screen.java
+++ Screen.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
