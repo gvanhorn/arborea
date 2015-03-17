2202d88345aed9210282d7de8afc21f881fa0fe8
gvanhorn <gijsvanhorn@gmail.com>
2015-03-17 10:47:49 +0100

    aanvallen als mogelijk

--- AggroTactic.java
+++ AggroTactic.java
@@ -45,28 +45,25 @@ public class AggroTactic extends Tactic{
 			
 			Unit target;
 			
-			if(adjacentEnemies.size() >= 1){
+			if(adjacentEnemies.size() > 0){
 				target = adjacentEnemies.get(0);
-				
-				
-				if(adjacentEnemies.size() > 1){
-					
-					//System.out.println("Choosing between multiple targets");
-					
-					for(Unit t : adjacentEnemies){
-						if((t.weaponSkill + t.weaponSkillModifier) < (target.weaponSkill + target.weaponSkillModifier)){
-							target = t;
-						}
+				double bestHitChance = getHitChance(u, target);
+				double hitChance = getHitChance(u, target);
+				for(Unit t : adjacentEnemies){
+					hitChance = getHitChance(u, target);
+					if(hitChance > bestHitChance){
+						bestHitChance = hitChance;
 					}
-					
-					super.movelist.addToHexList(u.getPosition(), target.getPosition());
-					super.movelist.addToTypeList("attack");
 				}
-				//System.out.println("TARGET is: " + target.toString());
+				
+				super.movelist.addToHexList(u.getPosition(), target.getPosition());
+				super.movelist.addToTypeList("attack");
 			}
+			//System.out.println("TARGET is: " + target.toString());
 		}
-
-
+	}
 	
+	public double getHitChance(Unit attacker, Unit target){
+		return 1/(1+Math.pow(Math.E, (-0.4* ((attacker.weaponSkill + attacker.weaponSkillModifier)-(target.weaponSkill + target.weaponSkillModifier)))));
 	}
 }
